3553e9a687475776abdec7bb6a991781c1e41da9
Andrey_BespaloFF <andrey2802@mail.ru>
2023-02-23 18:59:05 +0300

    add example_008

---
+++ example_008_/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 8;
+int c = 4;
+int d = 3;
+int e = 5;
+int f = 2;
+
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+
+Console.Write("max = ");
+Console.WriteLine(max);
