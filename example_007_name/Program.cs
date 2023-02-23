8cfe8945e7171d2ba5b113da924cd868ef73518d
Andrey_BespaloFF <andrey2802@mail.ru>
2023-02-23 18:46:37 +0300

    add example_007_didn't finish

---
+++ example_007_name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user NAME:  ");
+string username = Console.ReadLine();
+
+if(username == "ANDREW");
+{
+Console.WriteLine("YES, It's ANDREW!");
+}
+else
+{
+    Console.Write("Hello,  ");
+    Console.WriteLine(username);
+}
